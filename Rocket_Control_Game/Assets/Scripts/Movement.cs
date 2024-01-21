4683ff68084e5b2e5d48d60b6db6a8866abf515a
Mpprabhu <prabhu92004@gmail.com>
2024-01-21 22:32:27 +0530

    Rocket game

---
+++ Rocket_Control_Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]float thrust=1000f,rotatespeed=100f;
+    // [SerializeField]float rotatespeed = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThruster();   
+        ProcessRotation();     
+    }
+    void ProcessThruster()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up*thrust*Time.deltaTime);
+        }
+    }
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotatespeed);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotatespeed);
+
+        }
+    }
+
+    void ApplyRotation(float RotationValue)
+    {
+        rb.freezeRotation = true; //freezing physics for good rotation
+        transform.Rotate(Vector3.forward * RotationValue * Time.deltaTime); //manual control for rotation
+        rb.freezeRotation = false; //unfreezing after the completion of manual control
+    }
+}
